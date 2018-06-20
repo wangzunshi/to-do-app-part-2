5806f86b54252ce12c0d8c8dce7d7a7dd35b257d
kevin <kevin@example.com>
2018-06-20 15:58:32 -0400

    js-dom-2-checkpoint finished

--- app.js
+++ app.js
@@ -1,59 +1,49 @@
 function onReady() {
 
+  const toDos = [];
+
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+
+  function createNewToDo () {
+    const newToDoText = document.getElementById('newToDoText');
+
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+    newToDoText.value = '';
+
+    renderTheUI();
+  };
 
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-
-    // get text
-    let task = newToDoText.value;
-
-    //create new html li tag
-    let newLi = document.createElement('li');
-
-    //create new input tag
-    let newInput = document.createElement('input');
-
-
-    //input tag type to checkbox
-    newInput.type = "checkbox";
-
-    //list the task at the bottom
-    newLi.textContent = task;
-
-    // insiide new list tag is new input tag
-    newLi.appendChild(newInput);
-
-    //inside ul tag is list tag
-    toDoList.appendChild(newLi);
-
-    //empty input after the form is submitted
-    newToDoText.value = '';
-
-
-
-
-    let remove = document.createElement('button');
-
-    let removename = document.createTextNode("Delete");
-
-    remove.class = ("mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent");
-
-    remove.appendChild(removename);
-
-    newLi.appendChild(remove);
-
-    remove.addEventListener("click", function (){
-
-      toDoList.removeChild (newLi);
-
+    createNewToDo();
   });
 
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+
     });
-}
+
+  };
+
+};
 
 window.onload = function() {
 
