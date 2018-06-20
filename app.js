ec72e853eba5624350c7a451d99587a096e1a377
kevin <kevin@example.com>
2018-06-20 16:43:03 -0400

    first attempt

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 function onReady() {
 
+//------------step 1-----------//
+let id = 0;
+//----------------------//
+
   const toDos = [];
 
   const addToDoForm = document.getElementById('addToDoForm');
@@ -11,12 +15,20 @@ function onReady() {
 
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+
+      //------step 2-----------------//
+      id: id.value
+      //----------------------//
+
     });
+    //---------step 3-----------//
+    id++;
+    //-------------------//
     newToDoText.value = '';
 
     renderTheUI();
-  };
+  }
 
 
   addToDoForm.addEventListener('submit', event => {
@@ -39,13 +51,43 @@ function onReady() {
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
 
+      //--------------step 4----------------//
+
+
+          let remove = document.createElement('button');
+
+          let removename = document.createTextNode("Delete");
+
+          remove.appendChild(removename);
+
+          newLi.appendChild(remove);
+
+          remove.addEventListener("click", function (){
+
+            // toDoList.removeChild (newLi);
+
+            //---------------step 5------------//
+            toDos.filter(toDos.id);
+
+            //--------------------------------//
+
+
+            // ------------step 6--------------//
+            renderTheUI();
+
+            //---------------------------------//
+
+          });
+
+      //-----------------------------------//
+
     });
 
-  };
+  }
 
-};
+}
 
 window.onload = function() {
 
   onReady();
-}
+};
